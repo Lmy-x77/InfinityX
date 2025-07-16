c24056835bc89e52dcd0974873bb3bd163f49e1b
Lmy77 <lmy77infinityx@gmail.com>
2025-07-16 12:07:52 -0300

    Update Bypass.lua

--- games/Anime-Mania/Bypass.lua
+++ games/Anime-Mania/Bypass.lua
@@ -1,26 +1,44 @@
-local remoteName = "Ban"
-local mt = getrawmetatable(game)
-local lscript = game:GetService("Players").LocalPlayer.Backpack:WaitForChild("ClientMain")
-local senv = getsenv(lscript)
+print('[ BYPASS ] loading...')
+wait(2)
 
-setreadonly(mt, false)
-  local oldNamecall = mt.__namecall
-  mt.__namecall = newcclosure(function(self, ...)
-      local method = getnamecallmethod()
-      if method == "FireServer" and tostring(self) == remoteName then
-          return
-      end
-      return oldNamecall(self, ...)
-  end)
+pcall(function()
+    local v_u_1 = game.Players.LocalPlayer
+    local v_u_2 = v_u_1.Character or v_u_1.CharacterAdded:wait()
+    local bypass;
+    bypass = hookmetamethod(game, "__namecall", function(method, ...)
+        if getnamecallmethod() == "FireServer" and method == v_u_2:FindFirstChild('Ban') then
+            return
+        end
+        return bypass(method, ...)
+    end)
+    print('[ BYPASS ] - Block ban remote 🟢')
 
-  hookfunction(game.Players.LocalPlayer.Kick, newcclosure(function(...)
-      return nil
-  end))
-  mt.__namecall = newcclosure(function(self, ...)
-    local method = getnamecallmethod()
-    if self == game.Players.LocalPlayer and method == "Kick" then
-        return nil
+
+    local lscript = game:GetService("Players").LocalPlayer.Backpack:WaitForChild("ClientMain")
+    local senv = getsenv(lscript)
+    for k, v in pairs(senv) do
+        if typeof(v) == "function" then
+            local info = debug.getconstants(v)
+            for _, const in ipairs(info) do
+                if tostring(const):lower():find("kick") then
+                    senv[k] = function() end
+                    break
+                end
+            end
+        end
     end
-    return oldNamecall(self, ...)
-  end)
-setreadonly(mt, true)
+    print('[ BYPASS ] - Remove kick functions 🟢')
+
+
+    local lp = game:GetService("Players").LocalPlayer
+    local oldKick
+    oldKick = hookmetamethod(game, "__namecall", function(self, ...)
+        if self == lp and getnamecallmethod() == "Kick" then
+            return
+        end
+        return oldKick(self, ...)
+    end)
+    print('[ BYPASS ] - Anti kick security 🟢')
+end)
+
+print('[ BYPASS ] applied successfully')
