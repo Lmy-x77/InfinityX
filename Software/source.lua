06aed82bc5b3e535ee56bb261f1d48db7d7d0964
Lmy77 <lmy77infinityx@gmail.com>
2025-07-16 09:25:17 -0300

    Update source.lua

--- Software/source.lua
+++ Software/source.lua
@@ -6,17 +6,17 @@ wait(.8)
 -- variables
 local currentPlaceId = game.PlaceId
 local supportedGames = {
-    [{893973440}] = 'https://raw.githubusercontent.com/Lmy-77/InfinityX/refs/heads/scripts/games/FleeTheFacility/src.lua',
-    [{10260193230}] = 'https://raw.githubusercontent.com/Lmy-77/InfinityX/refs/heads/scripts/games/MemeSea/src.lua',
-    [{1962086868, 3582763398, 2127551566}] = 'https://raw.githubusercontent.com/Lmy-77/InfinityX/refs/heads/scripts/games/TowerOfHell/source.lua',
-    [{6284881984, 6314042276}] = 'https://raw.githubusercontent.com/Lmy-77/InfinityX/refs/heads/scripts/games/AnimeMania/source.lua',
-    [{72992062125248}] = 'https://raw.githubusercontent.com/Lmy-77/InfinityX/refs/heads/scripts/games/Hunters/source.lua',
-    [{126884695634066}] = "https://raw.githubusercontent.com/Lmy-77/InfinityX/refs/heads/scripts/games/GrowAGarden/source.lua",
-    [{17850641257, 17850769550}] = 'https://raw.githubusercontent.com/Lmy-77/InfinityX/refs/heads/scripts/games/AnimeSaga/source.lua',
-    [{75852144330025}] = 'https://raw.githubusercontent.com/Lmy-77/InfinityX/refs/heads/scripts/games/HuzzRng/source.lua',
-    [{734159876, 8908228901}] = 'https://raw.githubusercontent.com/Lmy-77/InfinityX/refs/heads/scripts/games/SharkBite/Loader.lua',
-    [{5610197459}] = 'https://raw.githubusercontent.com/Lmy-77/InfinityX/refs/heads/scripts/games/GangUpOnPeopleSimulator/source.lua',
-    [{126244816328678}] = 'https://raw.githubusercontent.com/Lmy-77/InfinityX/refs/heads/scripts/games/Dig/source.lua'
+    [{893973440}] = 'https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Flee-The-Facility/source.lua',
+    [{10260193230}] = 'https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Meme-Sea/source.lua',
+    [{1962086868, 3582763398, 2127551566}] = 'https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Tower-of-Hell/source.lua',
+    [{6284881984, 6314042276}] = 'https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Anime-Mania/source.lua',
+    [{72992062125248}] = 'https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Hunters/source.lua',
+    [{126884695634066}] = "https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Grow-a-Garden/source.lua",
+    [{17850641257, 17850769550}] = 'https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Anime-Saga/source.lua',
+    [{75852144330025}] = 'https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Huzz-Rng/source.lua',
+    [{734159876, 8908228901}] = 'https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Shark-Bite/source.lua',
+    [{5610197459}] = 'https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Gang-Up-On-People-Simulator/source.lua',
+    [{126244816328678}] = 'https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Dig/source.lua'
 }
 local function findScript()
     for key, scriptUrl in pairs(supportedGames) do
