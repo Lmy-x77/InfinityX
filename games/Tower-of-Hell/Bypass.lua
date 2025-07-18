61fe935c1cdc9791f0a7f024dab6b03a51b7f3af
Lmy77 <lmy77infinityx@gmail.com>
2025-07-17 21:22:29 -0300

    Update Bypass.lua

--- games/Tower-of-Hell/Bypass.lua
+++ games/Tower-of-Hell/Bypass.lua
@@ -11,20 +11,29 @@ end
 
 
 -- source
-if not hookfunction and getconnections and getsenv then
-  print('[ BYPASS ] - Failed ❌')
-else
-  disconnectAll(ls2.Changed)
-  hookfunction(func, function(...)
-      return nil
-  end)
-  for _, conn in pairs(getconnections(ls1.Changed)) do
-      conn:Disable()
-  end
-  task.spawn(function() while true do task.wait() ls1.Disabled = true ls2.Disabled = true end end)
+print('[ BYPASS ] - Loading...')
+task.wait(1.5)
+pcall(function()
+  if not hookfunction and getconnections and getsenv then
+    print('[ BYPASS ] - Failed ❌')
+  else
+    disconnectAll(ls2.Changed)
+    hookfunction(func, function(...)
+        return nil
+    end)
+    for _, conn in pairs(getconnections(ls1.Changed)) do
+        conn:Disable()
+    end
+    task.spawn(function() while true do task.wait() ls1.Disabled = true ls2.Disabled = true end end)
 
-  print('[ BYPASS ] - Disconnected functions ✅')
-  print('[ BYPASS ] - Hokked kick functions ✅')
-  print('[ BYPASS ] - Disabled changed function ✅')
-  print('[ BYPASS ] - Auto disable local scripts ✅')
-end
+    print('[ BYPASS ] - Disconnected functions ✅')
+    task.wait(.5)
+    print('[ BYPASS ] - Hokked kick functions ✅')
+    task.wait(.5)
+    print('[ BYPASS ] - Disabled changed function ✅')
+    task.wait(.5)
+    print('[ BYPASS ] - Auto disable local scripts ✅')
+  end
+end)
+task.wait(.25)
+print('[ BYPASS ] applied successfully')
