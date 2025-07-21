c69caaaf8d9071eee20c509b867e00c7dcb761b3
Lmy77 <lmy77infinityx@gmail.com>
2025-07-21 10:35:54 -0300

    Update Game1.lua

--- games/Shark-Bite/Place-Id/Game1.lua
+++ games/Shark-Bite/Place-Id/Game1.lua
@@ -77,13 +77,13 @@ local function connectPlayer(player)
     end
 end
 local LibrarySettings = {
-    Title = '<font color="rgb(110, 48, 160)" size="24"><b>InfinityX</b></font>',
+    Title = '<font color="rgb(110, 48, 160)" size="' .. (IsOnMobile and '18' or '24') .. '"><b>InfinityX</b></font>',
     Footer = {
-      GameName = '<font color="rgb(180,180,255)"><i>Shark Bite Classic</i></font> · ',
+      GameName = '<font color="rgb(180,180,255)"><i>Anime Mania [ Lobby ]</i></font> · ',
       Version = '<font color="rgb(160,160,160)">Version 4.2a</font> · ',
       DiscordLink = '<font color="rgb(100,200,255)">Join us: discord.gg/emKJgWMHAr</font>'
     }
-  }
+}
 
 
 
