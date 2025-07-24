655ed7851b383a02ba9638dd8661500039714e12
Lmy77 <lmy77infinityx@gmail.com>
2025-07-24 15:26:13 -0300

    Update source.lua

--- games/Tower-of-Hell/source.lua
+++ games/Tower-of-Hell/source.lua
@@ -425,7 +425,7 @@ UiSettingsGroubBox:AddButton("Unload", function()
 	Library:Unload()
 end)
 CreditsGroupBox:AddLabel("Script made by Lmy77")
-CreditsGroupBox:AddButton("Discor server", function()
+CreditsGroupBox:AddButton("Discord server", function()
 	setclipboard("https://discord.gg/emKJgWMHAr")
   Library:Notify({
     Title = "InfinityX",
