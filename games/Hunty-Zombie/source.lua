87eeb8ae6c6714666a16352aff9da1b1f914abd2
Lmy77 <lmy77infinityx@gmail.com>
2025-10-31 21:05:35 -0300

    Update source.lua

--- games/Hunty-Zombie/source.lua
+++ games/Hunty-Zombie/source.lua
@@ -1,8 +1,20 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Pixeluted/adoniscries/main/Source.lua",true))()
+
+for _, v in pairs(game.CoreGui:GetDescendants()) do
+    if v.Name == 'NotificationFrame' or v.Name == 'NotificationGui' then
+        v:Destroy()
+    end
+end
+game.CoreGui.DescendantAdded:Connect(function(v)
+    if v.Name == 'NotificationFrame' or v.Name == 'NotificationGui' then
+        v:Destroy()
+    end
+end)
+
 local placeid = { [1] = 103754275310547, [2] = 86076978383613}
 local currentPlace = game.PlaceId
 if currentPlace == placeid[1] then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Hunty-Zombie/Places/Place1.lua",true))()
-
 elseif currentPlace == placeid[2] then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Hunty-Zombie/Places/Place2.lua",true))()
 end
