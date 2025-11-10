ed4b97c6d673e53d7fa8a30d996c51cb19861df6
Lmy77 <lmy77infinityx@gmail.com>
2025-11-10 17:27:58 -0300

    Create exemple.lua

---
+++ Remote-Interceptor/exemple.lua
@@ -0,0 +1,5 @@
+local RemoteTracker = loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/library/Remote-Tracker/source.lua'))()
+RemoteTracker:Hook({
+    Path = nil, -- Remote path
+    Type = nil  -- Type of remote (RemoteEvent or RemoteFunction)
+})
