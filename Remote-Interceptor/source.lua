7d97a75c10b35e04235a5cbdd23e5eb386e35707
Lmy77 <lmy77infinityx@gmail.com>
2025-11-10 17:24:01 -0300

    Create source.lua

---
+++ Remote-Interceptor/source.lua
@@ -0,0 +1,32 @@
+local hook = {}
+hook.__index = hook
+
+local OldNamecall
+local OldInvoke
+
+function hook:RemoteInterceptor(info)
+    local Path = info.Path
+    local Type = info.Type
+
+    if Type == "RemoteEvent" then
+        OldNamecall = OldNamecall or hookmetamethod(game, "__namecall", function(self, ...)
+            local method = getnamecallmethod()
+            if self == Path and method == "FireServer" then
+                print("[Intercepted RemoteEvent]:", self.Name, ...)
+                return
+            end
+            return OldNamecall(self, ...)
+        end)
+    elseif Type == "RemoteFunction" then
+        if not Path or not Path.InvokeServer then return end
+        OldInvoke = OldInvoke or hookfunction(Path.InvokeServer, function(self, ...)
+            if self == Path then
+                print("[Intercepted RemoteFunction]:", self.Name, ...)
+                return
+            end
+            return OldInvoke(self, ...)
+        end)
+    end
+end
+
+return hook
