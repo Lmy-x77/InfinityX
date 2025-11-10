0204fe38e58a5fcf610ff38dbf255f509cca50d1
Lmy77 <lmy77infinityx@gmail.com>
2025-11-10 20:49:13 -0300

    Update source.lua

--- Remote-Tracker/source.lua
+++ Remote-Tracker/source.lua
@@ -1,34 +1,39 @@
 local hook = {}
 hook.__index = hook
 
-local OldNamecall = nil
-local OldInvoke = {}
+local OldNamecall
+local OldInvokeServer
 
 function hook:Hook(info)
     local Path = info.Path
     local Type = info.Type
     if not Path or not Type then return end
 
-    if Type == "RemoteEvent" then
-        if not OldNamecall then
-            OldNamecall = hookmetamethod(game, "__namecall", function(self, ...)
-                local method = getnamecallmethod()
-                if method == "FireServer" and tostring(self) == tostring(Path) then
-                    return
-                end
-                return OldNamecall(self, ...)
-            end)
-        end
+    if Type == "RemoteFunction" then
+        OldNamecall = OldNamecall or hookmetamethod(game, "__namecall", function(self, ...)
+            local method = getnamecallmethod()
+            if method == "InvokeServer" and self == Path then
+                return nil
+            end
+            return OldNamecall(self, ...)
+        end)
 
-    elseif Type == "RemoteFunction" then
-        if not OldInvoke[Path] then
-            OldInvoke[Path] = hookfunction(Path.InvokeServer, function(self, ...)
-                if tostring(self) == tostring(Path) then
+        if not OldInvokeServer then
+            OldInvokeServer = hookfunction(Path.InvokeServer, function(self, ...)
+                if self == Path then
                     return nil
                 end
-                return OldInvoke[Path](self, ...)
+                return OldInvokeServer(self, ...)
             end)
         end
+    elseif Type == "RemoteEvent" then
+        OldNamecall = OldNamecall or hookmetamethod(game, "__namecall", function(self, ...)
+            local method = getnamecallmethod()
+            if method == "FireServer" and self == Path then
+                return
+            end
+            return OldNamecall(self, ...)
+        end)
     end
 end
 
