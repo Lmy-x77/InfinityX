281386700bdbac06b06bf8f6b3d13a1dc4556f88
Lmy77 <lmy77infinityx@gmail.com>
2025-11-10 17:28:16 -0300

    Delete Remote-Interceptor directory

--- Remote-Interceptor/exemple.lua
+++
@@ -1,5 +0,0 @@
-local RemoteTracker = loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/library/Remote-Tracker/source.lua'))()
-RemoteTracker:Hook({
-    Path = nil, -- Remote path
-    Type = nil  -- Type of remote (RemoteEvent or RemoteFunction)
-})
