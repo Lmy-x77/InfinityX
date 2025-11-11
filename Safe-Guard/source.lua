68fee96f2d902ccde22b940e55de88aea8a09e1c
Lmy77 <lmy77infinityx@gmail.com>
2025-11-11 19:48:45 -0300

    Update source.lua

--- Safe-Guard/source.lua
+++ Safe-Guard/source.lua
@@ -1,1 +1,24 @@
--- in dev
+local SafeGuard = {}
+SafeGuard.__index = SafeGuard
+
+function SafeGuard:Hook(info)
+  local AntiKick = info.AntiKick
+  local AntiBan = info.AntiBan
+  local AntiHttpSpy = info.AntiHttpSpy
+  local BlockRemote = info.BlockRemote or {}
+
+  if AntiKick then
+    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/library/Safe-Guard/script/Anti-Kick.lua'))()
+  end
+  if AntiBan then
+    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/library/Safe-Guard/script/Anti-Ban.lua'))()
+  end
+  if AntiHttpSpy then
+    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/library/Safe-Guard/script/Http-Spy.lua'))()
+  end
+  if BlockRemote.Enabled then
+    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/library/Safe-Guard/script/Block-Remote.lua'))()
+  end
+end
+
+return SafeGuard
