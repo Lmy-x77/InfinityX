ebcfcd9e95a2da502975d08d54896b3b07ed6e50
Lmy77 <lmy77infinityx@gmail.com>
2025-11-12 18:55:36 -0300

    Create exemple.lua

---
+++ Fluriore/exemple.lua
@@ -0,0 +1,85 @@
+local FlurioreLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/library/Fluriore/source.lua"))()
+local FlurioreGui = FlurioreLib:MakeGui({
+    ["NameHub"] = "InfinityX - v4.2a -",
+    ["Description"] = "by lmy77 ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ",
+    ["Color"] = Color3.new(0.600000, 0.000000, 1.000000),
+    ["Logo Player"] = "https://www.roblox.com/headshot-thumbnail/image?userId="..game:GetService("Players").LocalPlayer.UserId .."&width=420&height=420&format=png",
+    ["Name Player"] = tostring(game:GetService("Players").LocalPlayer.Name),
+    ["Tab Width"] = 140
+})
+local Tab1 = FlurioreGui:CreateTab({
+    ["Name"] = "Main",
+    ["Icon"] = "rbxassetid://7733960981"
+})
+local Tab2 = FlurioreGui:CreateTab({
+    ["Name"] = "Setting",
+    ["Icon"] = "rbxassetid://7734053495"
+})
+local Section = Tab1:AddSection("Setting Farm")
+local Paragraph = Section:AddParagraph({
+    ["Title"] = "Paragraph",
+    ["Content"] = "This is a Paragraph"
+})
+Paragraph:Set({
+    ["Title"] = "Paragraph",
+    ["Content"] = "This is a Paragraph"
+})
+local Toggle = Section:AddToggle({
+    ["Title"]= "Toggle",
+    ["Content"] = "This is a Toggle",
+    ["Default"] = false,
+    ["Multi"] = true,
+    ["Options"] = {"Option 1", "Option 2"},
+    ["Selecting"] = {"Option 1"},
+    ["Callback"] = function(Value) 
+        print(Value)
+    end
+})
+Toggle:Set(false)
+print(Toggle.Value)
+local Button = Section:AddButton({
+    ["Title"] = "Button",
+    ["Content"] = "This is a Button",
+    ["Icon"] = "rbxassetid://16932740082",
+    ["Callback"] = function()
+        print("Button Clicked!")
+    end
+})
+local Slider = Section:AddSlider({
+    ["Title"] = "Slider",
+    ["Content"] = "This is a Slider",
+    ["Min"] = 0,
+    ["Max"] = 100,
+    ["Increment"] = 1,
+    ["Default"] = 30,
+    ["Callback"] = function(Value) 
+        print(Value)
+    end
+})
+Slider:Set(30)
+print(Slider.Value)
+local Input = Section:AddInput({
+    ["Title"] = "Input",
+    ["Content"] = "This is a Input",
+    ["Callback"] = function(Value) 
+        print(Value)
+    end
+})
+Input:Set("Input TextBox")
+print(Input.Value)
+local Dropdown = Section:AddDropdown({
+    ["Title"] = "Dropdown",
+    ["Content"] = "This is a Dropdown",
+    ["Multi"] = false,
+    ["Options"] = {"Option 1", "Option 2"},
+    ["Default"] = {"Option 1"},
+    ["Callback"] = function(Value)
+        print(Value)
+    end
+})
+Dropdown:Set({"Option 1"})
+Dropdown:AddOption("Option 3")
+Dropdown:Clear()
+Dropdown:Refresh({"Option 1", "Option 2"}, {"Option 1"})
+print(Dropdown.Value)
+print(Dropdown.Options)
