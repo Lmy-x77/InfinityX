cc4bde3a5f7128ae61a781b6f282edffaded271b
Lmy77 <lmy77infinityx@gmail.com>
2026-01-05 22:01:24 -0300

    Update source.lua

--- games/Blair/source.lua
+++ games/Blair/source.lua
@@ -461,7 +461,7 @@ EspLib.ESPValues.AllItemsESP = false
 
 -- ui library
 local isMobile = game.UserInputService.TouchEnabled
-local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/library/Fluent/source.lua"))()
+local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/library/Fluent/main.lua"))()
 
 local Options = Fluent.Options
 
