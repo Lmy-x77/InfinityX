06ab20e57a899624c7cb752a677fdcd297133e5f
Lmy77 <lmy77infinityx@gmail.com>
2026-01-07 16:33:10 -0300

    Update source.lua

--- games/Hunty-Zombie/source.lua
+++ games/Hunty-Zombie/source.lua
@@ -14,7 +14,7 @@ end)
 local placeid = { [1] = 103754275310547, [2] = 86076978383613}
 local currentPlace = game.PlaceId
 if currentPlace == placeid[1] then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Hunty-Zombie/Places/Place1.lua",true))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Hunty-Zombie/Places/lobby.lua",true))()
 elseif currentPlace == placeid[2] then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Hunty-Zombie/Places/Place2.lua",true))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Hunty-Zombie/Places/arena.lua",true))()
 end
