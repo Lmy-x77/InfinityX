c387624d204466b6d3f504af2b828f2288ccf0e7
Lmy77 <lmy77infinityx@gmail.com>
2026-01-10 10:04:37 -0300

    Create bypass.lua

---
+++ games/Anime-Fighinting-Simulator-Endless/bypass.lua
@@ -0,0 +1,97 @@
+local GameData = game:GetService("ReplicatedStorage").Modules.GameData
+local success, source = pcall(require, GameData)
+if not success then return end
+
+for _, f in pairs(source) do
+    if type(f) == "function" then
+        for _, c in pairs(debug.getconstants(f)) do
+            if type(c) == "string" and c:lower() == "kick" then
+                hookfunction(f, function() return end)
+                break
+            end
+        end
+    end
+end
+
+task.spawn(function()
+    local RF = game:GetService("ReplicatedFirst")
+    local start, ls2
+
+    repeat
+        start = RF:FindFirstChild("Start")
+        task.wait(1)
+    until start
+
+    repeat
+        ls2 = start:FindFirstChild("LocalScript2")
+        task.wait(1)
+    until ls2
+
+    for i = 1, 5 do task.wait(3)
+        for _, v in pairs(getgc(false)) do
+            if type(v) == "function" then
+                local info = debug.getinfo(v)
+                if info and info.name == "reportDetectionAndCrash" then
+                    hookfunction(v, function() return end)
+                    return
+                end
+            end
+        end
+    end
+end)
+
+task.spawn(function()
+    while true do task.wait(1)
+        local gui = game.Players.LocalPlayer.PlayerGui:FindFirstChild("ImpactFrame")
+        if gui then
+            local ls = gui:FindFirstChildWhichIsA("LocalScript")
+            if ls then
+                for _, f in pairs(getsenv(ls)) do
+                    if type(f) == "function" then
+                        hookfunction(f, function() return end)
+                        break
+                    end
+                end
+            end
+            for _, s in pairs(gui:GetDescendants()) do
+                if s:IsA("Sound") then
+                    s:Destroy()
+                end
+            end
+            gui:Destroy()
+        end
+    end
+end)
+
+task.spawn(function()
+    local RF = game:GetService("ReplicatedFirst")
+    local start, ls2
+
+    repeat
+        start = RF:FindFirstChild("Start")
+        task.wait(1)
+    until start
+
+    repeat
+        ls2 = start:FindFirstChild("LocalScript2")
+        task.wait(1)
+    until ls2
+
+    for _, f in pairs(getsenv(ls2)) do
+        if type(f) == "function" then
+            hookfunction(f, function() return end)
+        end
+    end
+
+    local module = ls2:FindFirstChild("ModuleScript")
+    if module then
+        local ok, mod = pcall(require, module)
+        if ok then
+            for _, f in pairs(mod) do
+                if type(f) == "function" then
+                    hookfunction(f, function() return end)
+                end
+            end
+        end
+    end
+end)
