587c0e6138d09b5c9c4229da337f1a782def39f8
Lmy77 <lmy77infinityx@gmail.com>
2026-01-11 15:52:49 -0300

    Update quest.lua

--- games/Anime-Fighinting-Simulator-Endless/modules/quest.lua
+++ games/Anime-Fighinting-Simulator-Endless/modules/quest.lua
@@ -1,1 +1,157 @@
+if SelectedStat == 'Strength' and Strength.Value >= 100 and Strength.Value < 10000 then
+  Teleport(nil, 2, -6, 71, 134)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 1)
+elseif SelectedStat == 'Strength' and Strength.Value >= 10000 and Strength.Value < 100000 then
+  Teleport(1343, 195, -141)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 1)
+elseif SelectedStat == 'Strength' and Strength.Value >= 100000 and Strength.Value < 1000000 then
+  Teleport(nil, 2, -1257, 65, 486)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 1)
+elseif SelectedStat == 'Strength' and Strength.Value >= 1000000 and Strength.Value < 10000000 then
+  Teleport(nil, 2, -917, 85, 179)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 1)
+elseif SelectedStat == 'Strength' and Strength.Value >= 10000000 and Strength.Value < 100000000 then
+  Teleport(nil, 2, -2245, 617, 533)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 1)
+elseif SelectedStat == 'Strength' and Strength.Value >= 100000000 and Strength.Value < 1000000000 then
+  Teleport(nil, 2, -42, 65, -1248)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 1)
+elseif SelectedStat == 'Strength' and Strength.Value >= 1000000000 and Strength.Value < 100000000000 then
+  Teleport(nil, 2,721, 149, 925)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 1)
+elseif SelectedStat == 'Strength' and Strength.Value >= 100000000000 and Strength.Value < 5000000000000 then
+  Teleport(nil, 2, 1842, 139, 96)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 1)
+elseif SelectedStat == 'Strength' and Strength.Value >= 5000000000000 and Strength.Value < 250000000000000 then
+  Teleport(nil, 2, 621, 662, 413)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 1)
+elseif SelectedStat == 'Strength' and Strength.Value >= 250000000000000 and Strength.Value < 150000000000000000 then
+  Teleport(nil, 2, 4289, 163, -601)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 1)
+elseif SelectedStat == 'Strength' and Strength.Value >= 150000000000000000 and Strength.Value < 25000000000000000000 then
+  Teleport(nil, 2, 798, 231, -1004)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 1)
+elseif SelectedStat == 'Strength' and Strength.Value >= 25000000000000000000 and Strength.Value < 10000000000000000000000 then
+  Teleport(nil, 2, 3873, 138, 873)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 1)
+elseif SelectedStat == 'Strength' and Strength.Value > 10000000000000000000000 then
+  Teleport(nil, 2, 3858, 669, -1076)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 1)
 
+
+elseif SelectedStat == 'Durability' and Durability.Value >= 100 and Durability.Value < 10000 then
+  Teleport(nil, 2, 67, 69, 878)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 2)
+elseif SelectedStat == 'Durability' and Durability.Value >= 10000 and Durability.Value < 100000 then
+  Teleport(nil, 2, -1655, 63, -520)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 2)
+elseif SelectedStat == 'Durability' and Durability.Value >= 100000 and Durability.Value < 1000000 then
+  Teleport(nil, 2, -93, 101, 2029)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 2)
+elseif SelectedStat == 'Durability' and Durability.Value >= 1000000 and Durability.Value < 10000000 then
+  Teleport(nil, 2, -628, 179, 720)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 2)
+elseif SelectedStat == 'Durability' and Durability.Value >= 10000000 and Durability.Value < 100000000 then
+  Teleport(nil, 2, -1108, 211, -962)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 2)
+elseif SelectedStat == 'Durability' and Durability.Value >= 100000000 and Durability.Value < 1000000000 then
+  Teleport(nil, 2, -333, 72, -1650)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 2)
+elseif SelectedStat == 'Durability' and Durability.Value >= 1000000000 and Durability.Value < 100000000000 then
+  Teleport(nil, 2, 2508, 1543, -380)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 2)
+elseif SelectedStat == 'Durability' and Durability.Value >= 100000000000 and Durability.Value < 5000000000000 then
+  Teleport(nil, 2, -2802, -228, 355)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 2)
+elseif SelectedStat == 'Durability' and Durability.Value >= 5000000000000 and Durability.Value < 250000000000000 then
+  Teleport(nil, 2, 2187, 517, 581)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 2)
+elseif SelectedStat == 'Durability' and Durability.Value >= 250000000000000 and Durability.Value < 150000000000000000 then
+  Teleport(nil, 2, 1671, 423, -1293)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 2)
+elseif SelectedStat == 'Durability' and Durability.Value >= 150000000000000000 and Durability.Value < 25000000000000000000 then
+  Teleport(nil, 2, 155, 772, -699)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 2)
+elseif SelectedStat == 'Durability' and Durability.Value >= 25000000000000000000 and Durability.Value < 10000000000000000000000 then
+  Teleport(nil, 2, 2565, 267, 1851)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 2)
+elseif SelectedStat == 'Durability' and Durability.Value > 10000000000000000000000 then
+  Teleport(nil, 2, 1673, 2305, -78)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 2)
+
+
+elseif SelectedStat == 'Chakra' and Chakra.Value >= 100 and Chakra.Value < 10000 then
+  Teleport(nil, 2, 4, 64, -124)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 3)
+elseif SelectedStat == 'Chakra' and Chakra.Value >= 10000 and Chakra.Value < 100000 then
+  Teleport(nil, 2, 1424, 146, -581)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 3)
+elseif SelectedStat == 'Chakra' and Chakra.Value >= 100000 and Chakra.Value < 1000000 then
+  Teleport(nil, 2, 914, 140, 794)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 3)
+elseif SelectedStat == 'Chakra' and Chakra.Value >= 1000000 and Chakra.Value < 10000000 then
+  Teleport(nil, 2, 1551, 387, 685)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 3)
+elseif SelectedStat == 'Chakra' and Chakra.Value >= 10000000 and Chakra.Value < 100000000 then
+  Teleport(nil, 2, 346, -149, -1844)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 3)
+elseif SelectedStat == 'Chakra' and Chakra.Value >= 100000000 and Chakra.Value < 1000000000 then
+  Teleport(nil, 2, 1022, 249, -618)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 3)
+elseif SelectedStat == 'Chakra' and Chakra.Value >= 1000000000 and Chakra.Value < 100000000000 then
+  Teleport(nil, 2, 3054, 110, 1105)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 3)
+elseif SelectedStat == 'Chakra' and Chakra.Value >= 100000000000 and Chakra.Value < 5000000000000 then
+  Teleport(nil, 2, 1710, 577, 1743)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 3)
+elseif SelectedStat == 'Chakra' and Chakra.Value >= 5000000000000 and Chakra.Value < 250000000000000 then
+  Teleport(nil, 2, -16, 61, -475)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 3)
+elseif SelectedStat == 'Chakra' and Chakra.Value >= 250000000000000 and Chakra.Value < 25000000000000000 then
+  Teleport(nil, 2, -411, 1255, 663)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 3)
+elseif SelectedStat == 'Chakra' and Chakra.Value >= 25000000000000000 and Chakra.Value < 150000000000000000 then
+  Teleport(nil, 2, -411, 1255, 663) -- find
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 3)
+elseif SelectedStat == 'Chakra' and Chakra.Value >= 150000000000000000 and Chakra.Value < 25000000000000000000 then
+  Teleport(nil, 2, -732, 2791, 628)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 3)
+elseif SelectedStat == 'Chakra' and Chakra.Value >= 25000000000000000000 and Chakra.Value < 10000000000000000000000 then
+  Teleport(nil, 2, 3242, -441, -233)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 3)
+elseif SelectedStat == 'Chakra' and Chakra.Value >= 10000000000000000000000 then
+  Teleport(nil, 2, 341, 237, 1867)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 3)
+
+
+elseif SelectedStat == 'Agility' and Agility.Value >= 100 and Agility.Value < 10000 then
+  Teleport(nil, 2, 37, 69, 459)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 5)
+elseif SelectedStat == 'Agility' and Agility.Value >= 10000 and Agility.Value < 100000 then
+  Teleport(nil, 2, -424, 122, -81)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 5)
+elseif SelectedStat == 'Agility' and Agility.Value >= 100000 and Agility.Value < 5000000 then
+  Teleport(nil, 2, 3479, 60, 143)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 5)
+elseif SelectedStat == 'Agility' and Agility.Value >= 5000000 then
+  Teleport(nil, 2, 4111, 69, 879)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 5)
+
+
+elseif SelectedStat == 'Speed' and Speed.Value >= 100 and Speed.Value < 10000 then
+  Teleport(nil, 2, -102, 62, -496)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 6)
+elseif SelectedStat == 'Speed' and Speed.Value >= 10000 and Speed.Value < 100000 then
+  Teleport(nil, 2, -424, 122, -81)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 6)
+elseif SelectedStat == 'Speed' and Speed.Value >= 100000 and Speed.Value < 5000000 then
+  Teleport(nil, 2, 3479, 60, 143)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 6)
+elseif SelectedStat == 'Speed' and Speed.Value >= 5000000 then
+  Teleport(nil, 2, 4111, 69, 879)
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 6)
+
+
+elseif SelectedStat == 'Sword' then
+  game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("RemoteEvent"):FireServer('Train', 4)
+end
