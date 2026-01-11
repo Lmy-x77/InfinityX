fccb08683e52f0cbf8d23e951e2541dccf0606be
Lmy77 <lmy77infinityx@gmail.com>
2026-01-11 20:46:23 -0300

    Update source.lua

--- games/Anime-Fighinting-Simulator-Endless/source.lua
+++ games/Anime-Fighinting-Simulator-Endless/source.lua
@@ -1,3 +1,10 @@
+game.Players.LocalPlayer:Kick('The script is not safe to run at this time. Please wait until we bypass it. For more information, join the Discord server.')
+return
+
+
+
+
+
 ---@diagnostic disable: undefined-global
 -- detect service
 local UserInputService = game:GetService("UserInputService")
