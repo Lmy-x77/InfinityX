a27e1568b87f3a2401b1b5910550c7e57b678818
Lmy77 <lmy77infinityx@gmail.com>
2026-02-06 17:23:38 -0300

    Update source.lua

--- games/Shark-Bite/source.lua
+++ games/Shark-Bite/source.lua
@@ -1,11 +1,1 @@
-local placeId = {
-  Game1 = 734159876,
-  Game2 = 8908228901
-}
-
-
-if game.PlaceId == placeId.Game1 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Shark-Bite/Place-Id/Game1.lua", true))()
-elseif game.PlaceId == placeId.Game2 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Shark-Bite/Place-Id/Game2.lua", true))()
-end
+-- hello
