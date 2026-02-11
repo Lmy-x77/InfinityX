c90fea199c134456d4505da62ded1065a176ed39
Lmy77 <lmy77infinityx@gmail.com>
2026-02-10 21:50:30 -0300

    Create ChampionsFunction.lua

---
+++ games/Anime-Fighinting-Simulator-Endless/modules/ChampionsFunction.lua
@@ -0,0 +1,70 @@
+local Champions = loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Anime-Fighinting-Simulator-Endless/modules/Champions.lua"))()
+
+local ChampionsModule = {}
+
+local Players = cloneref(game:GetService("Players"));
+local ReplicatedStorage = cloneref(game:GetService("ReplicatedStorage"));
+local RemoteF = ReplicatedStorage.shared.Remotes.RemoteFunction
+
+local LastSummon = 0
+local SUMMON_CD = 1.2
+local LastStat, LastEquipped
+
+local function CanSummon()
+	return os.clock() - LastSummon >= SUMMON_CD
+end
+
+local function Summon(champ)
+	if not champ or not CanSummon() then return end
+	LastSummon = os.clock()
+	RemoteF:InvokeServer("SummonChamp", champ)
+end
+
+function ChampionsModule.Refresh()
+	local plr = Players.LocalPlayer
+	if not plr then return end
+	local equipped = plr:FindFirstChild("ChampionEquipped")
+	if equipped and equipped.Value ~= "" then
+		local champ = plr.Champions:FindFirstChild(equipped.Value)
+		if champ then Summon(champ) end
+	end
+	LastEquipped, LastStat = nil, nil
+end
+
+function ChampionsModule.EquipBest(statId, ChampionsData)
+	if not getgenv().StatsFarm.EquipBestChampion then return end
+	local plr = Players.LocalPlayer
+	if not plr or not plr.Champions then return end
+
+	if LastStat and LastStat ~= statId then
+		ChampionsModule.Refresh()
+	end
+
+	local bestName, bestVal = nil, 0
+	for _,c in pairs(plr.Champions:GetChildren()) do
+		local data = ChampionsData[c.Name]
+		local at = data and data.Abilities and data.Abilities.AutoTrainer
+		local v = at and at[tostring(statId)]
+		if v and v > bestVal then
+			bestVal, bestName = v, c.Name
+		end
+	end
+
+	if not bestName then return end
+
+	local equipped = plr:FindFirstChild("ChampionEquipped")
+	if equipped and equipped.Value == bestName and LastStat == statId then return end
+
+	if equipped and equipped.Value ~= "" then
+		local old = plr.Champions:FindFirstChild(equipped.Value)
+		if old then Summon(old) end
+	end
+
+	local new = plr.Champions:FindFirstChild(bestName)
+	if new then Summon(new) end
+
+	LastEquipped = bestName
+	LastStat = statId
+end
+
+return ChampionsModule
