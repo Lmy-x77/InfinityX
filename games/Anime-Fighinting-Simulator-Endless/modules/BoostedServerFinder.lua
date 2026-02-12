b55f9ad4effb2affef54f15fcb70cb7dc4438b0b
Lmy77 <lmy77infinityx@gmail.com>
2026-02-12 16:06:43 -0300

    Update and rename FindBoostedServer.lua to BoostedServerFinder.lua

---
+++ games/Anime-Fighinting-Simulator-Endless/modules/BoostedServerFinder.lua
@@ -0,0 +1,165 @@
+local BoostedServerFinder = {}
+BoostedServerFinder.__index = BoostedServerFinder
+
+function BoostedServerFinder.new(minPlayers, maxPlayers, hopDelay)
+    local self = setmetatable({}, BoostedServerFinder)
+
+    self.HttpService = game:GetService("HttpService")
+    self.TeleportService = game:GetService("TeleportService")
+    self.Players = game:GetService("Players")
+    self.RunService = game:GetService("RunService")
+    self.ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+    self.MIN_PLAYERS = minPlayers or 1
+    self.MAX_PLAYERS = maxPlayers or 20
+    self.HOP_DELAY = hopDelay or 5
+
+    self.WEBHOOK_URLS = {}
+    self.sentWebhook = false
+    self.boostHopping = false
+
+    return self
+end
+
+function BoostedServerFinder:AddWebhook(urls)
+    for _, url in pairs(urls) do
+        if type(url) == "string" and url ~= "" then
+            table.insert(self.WEBHOOK_URLS, url)
+        end
+    end
+end
+
+function BoostedServerFinder:GetServerBoost()
+    local success, value = pcall(function()
+        return self.ReplicatedStorage.shared.ReplicatedData.Boosts.ServerBoost.Value
+    end)
+
+    if success and tonumber(value) and value > 0 then
+        return tonumber(value)
+    end
+
+    return 0
+end
+
+function BoostedServerFinder:FormatTime(seconds)
+    local totalSeconds = tonumber(seconds) or 0
+    local hours = math.floor(totalSeconds / 3600)
+    local minutes = math.floor((totalSeconds % 3600) / 60)
+    local secs = math.floor(totalSeconds % 60)
+
+    return string.format("%02dh %02dm %02ds", hours, minutes, secs)
+end
+
+function BoostedServerFinder:SendWebhook(boostValue, jobId, playerCount)
+    local formattedTime = self:FormatTime(boostValue)
+
+    for _, WEBHOOK_URL in pairs(self.WEBHOOK_URLS) do
+        local embed = {
+            ["embeds"] = {{
+                ["title"] = "Boosted Server Found!",
+                ["color"] = 65280,
+                ["fields"] = {
+                    {["name"] = "Boost Time", ["value"] = formattedTime, ["inline"] = true},
+                    {["name"] = "Players", ["value"] = playerCount .. "/" .. self.MAX_PLAYERS, ["inline"] = true},
+                    {["name"] = "Job ID", ["value"] = "```" .. jobId .. "```"},
+                    {
+                        ["name"] = "Join Command",
+                        ["value"] = "```lua\ngame:GetService('TeleportService'):TeleportToPlaceInstance(" 
+                            .. game.PlaceId .. ", '" .. jobId .. "')```"
+                    }
+                },
+                ["timestamp"] = os.date("!%Y-%m-%dT%H:%M:%S")
+            }}
+        }
+
+        pcall(function()
+            http_request({
+                Url = WEBHOOK_URL,
+                Method = "POST",
+                Headers = {["Content-Type"] = "application/json"},
+                Body = self.HttpService:JSONEncode(embed)
+            })
+        end)
+    end
+end
+
+function BoostedServerFinder:GetServers()
+    local servers = {}
+
+    local success, response = pcall(function()
+        local url = string.format(
+            "https://games.roblox.com/v1/games/%d/servers/Public?sortOrder=Asc&limit=100",
+            game.PlaceId
+        )
+        return http_request({Url = url, Method = "GET"}).Body
+    end)
+
+    if success and response then
+        local data = self.HttpService:JSONDecode(response)
+        for _, server in pairs(data.data or {}) do
+            if server.playing >= self.MIN_PLAYERS
+                and server.playing <= self.MAX_PLAYERS
+                and server.id ~= game.JobId then
+                table.insert(servers, server.id)
+            end
+        end
+    end
+
+    return servers
+end
+
+function BoostedServerFinder:Hop()
+    local servers = self:GetServers()
+
+    if #servers == 0 then
+        self.TeleportService:Teleport(game.PlaceId)
+        return
+    end
+
+    local jobId = servers[math.random(1, #servers)]
+    self.TeleportService:TeleportToPlaceInstance(game.PlaceId, jobId)
+end
+
+function BoostedServerFinder:VerifyIfServerIsBoosted()
+    local boostValue = self:GetServerBoost()
+
+    if boostValue > 0 then
+        if not self.sentWebhook then
+            self.sentWebhook = true
+            self.boostHopping = false
+
+            local playerCount = #self.Players:GetPlayers()
+            self:SendWebhook(boostValue, game.JobId, playerCount)
+        end
+        return true
+    end
+
+    return false
+end
+
+function BoostedServerFinder:Start()
+    if self.boostHopping then
+        return
+    end
+
+    self.boostHopping = true
+    self.sentWebhook = false
+
+    self.RunService.Heartbeat:Connect(function()
+        if not self.boostHopping then
+            return
+        end
+
+        if not self:VerifyIfServerIsBoosted() then
+            self:Hop()
+        end
+
+        task.wait(self.HOP_DELAY)
+    end)
+end
+
+function BoostedServerFinder:Stop()
+    self.boostHopping = false
+end
+
+return BoostedServerFinder
