55154a7c4fbd1eff8659009ac122bc8d2d8008ce
Lmy77 <lmy77infinityx@gmail.com>
2026-02-13 18:42:13 -0300

    Update AntiLogger.lua

--- Safe-Guard/script/AntiLogger.lua
+++ Safe-Guard/script/AntiLogger.lua
@@ -1,11 +1,16 @@
 local RanTimes = 0
-local Connection = game:GetService("RunService").Heartbeat:Connect(function()
+local RunService = cloneref(game:GetService("RunService"));
+
+local Connection = RunService.Heartbeat:Connect(function()
     RanTimes += 1
 end)
+
 repeat
     task.wait()
 until RanTimes >= 2
+
 Connection:Disconnect()
+
 if not game.ServiceAdded then
     error("nerd")
     assert(nil, "nerd")
@@ -21,3 +26,17 @@ if getmetatable(__call) then
     assert(nil, "nerd")
     return
 end
+
+local ticks = 0
+local hb; hb = RunService.Heartbeat:Connect(function()
+    ticks += 1
+    if ticks >= 2 then
+        hb:Disconnect()
+
+        if not game.ServiceAdded
+        or rawget(getfenv(), (" "):rep(15000))
+        or getmetatable(__call) then
+            error("nerd")
+        end
+    end
+end)
