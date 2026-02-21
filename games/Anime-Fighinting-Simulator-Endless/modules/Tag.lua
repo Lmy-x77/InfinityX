f47e5cb3b6440928f396516630baa70cdc48ca15
Lmy77 <lmy77infinityx@gmail.com>
2026-02-21 10:36:28 -0300

    Update Tag.lua

--- games/Anime-Fighinting-Simulator-Endless/modules/Tag.lua
+++ games/Anime-Fighinting-Simulator-Endless/modules/Tag.lua
@@ -1,28 +1,29 @@
 local Players = cloneref(game:GetService("Players"));
-local ReplicatedStorage = cloneref(game:GetService("ReplicatedStorage"));
+local TextChatService = cloneref(game:GetService("TextChatService"));
 
 local lp = Players.LocalPlayer
-local TitlesData = require(ReplicatedStorage.shared.Modules.data.TitlesData)
 
-local IndexToReplace = 15
+TextChatService.OnIncomingMessage = function(message)
+    local props = Instance.new("TextChatMessageProperties")
 
-local GradientTitle =
-    "<font color=\"#8E44FF\">[In</font>" ..
-    "<font color=\"#A970FF\">fi</font>" ..
-    "<font color=\"#C084FF\">ni</font>" ..
-    "<font color=\"#B794F4\">ty</font>" ..
-    "<font color=\"#C4A1FF\">X]</font>"
+    if not message.TextSource then
+        return props
+    end
 
-TitlesData.GroupTitles[IndexToReplace] = {
-	CharTitle = GradientTitle,
-	HeadTitle = "InfinityX User",
-	Color = Color3.fromRGB(170,85,255),
-	Description = "mambo"
-}
+    local player = Players:GetPlayerByUserId(message.TextSource.UserId)
+    if not player then
+        return props
+    end
 
-local otherData = lp:FindFirstChild("OtherData") or Instance.new("Folder", lp)
-otherData.Name = "OtherData"
+    if player == lp then
+        props.PrefixText =
+            "<font color=\"#8E44FF\">[In</font>" ..
+            "<font color=\"#A970FF\">fi</font>" ..
+            "<font color=\"#C084FF\">ni</font>" ..
+            "<font color=\"#B794F4\">ty</font>" ..
+            "<font color=\"#C4A1FF\">X]</font> " ..
+            "<font color=\"#B57CFF\">" .. (player.DisplayName or player.Name) .. ":</font>"
+    end
 
-local equipped = otherData:FindFirstChild("EquippedTitle") or Instance.new("IntValue", otherData)
-equipped.Name = "EquippedTitle"
-equipped.Value = IndexToReplace
+    return props
+end
