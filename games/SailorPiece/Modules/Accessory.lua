0785573911da112d7a1f479e264235abc7466e79
Lmy77 <lmy77infinityx@gmail.com>
2026-04-04 19:00:04 -0300

    Update and rename Accessory to Accessory.lua

---
+++ games/SailorPiece/Modules/Accessory.lua
@@ -0,0 +1,359 @@
+local Accessorys = {}
+Accessorys.__index = Accessorys
+
+local v1 = {}
+local v2 = {}
+local v3 = {
+    ["DisplayName"] = "Holy Armor",
+    ["stats"] = {
+        ["Damage"] = 20,
+        ["Defense"] = 35,
+        ["DamageReduction"] = 10
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0"
+}
+v2["Saber Armor"] = v3
+local v4 = {
+    ["DisplayName"] = "Sorcerer Blindfold",
+    ["stats"] = {
+        ["Damage"] = 25,
+        ["Defense"] = 40
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0"
+}
+v2["Gojo Blindfold"] = v4
+local v5 = {
+    ["DisplayName"] = "Vessel Hair",
+    ["stats"] = {
+        ["Damage"] = 22.5,
+        ["Defense"] = 30,
+        ["DamageReduction"] = 12.5
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0"
+}
+v2["Yuji Hair"] = v5
+local v6 = {
+    ["DisplayName"] = "Curse King Collar",
+    ["stats"] = {
+        ["Damage"] = 30,
+        ["Defense"] = 45
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0"
+}
+v2["Sukuna Collar"] = v6
+local v7 = {
+    ["DisplayName"] = "Hunter Cape",
+    ["stats"] = {
+        ["Damage"] = 30,
+        ["Defense"] = 40,
+        ["DamageReduction"] = 15
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0"
+}
+v2["Jinwoo Cape"] = v7
+local v8 = {
+    ["stats"] = {
+        ["Damage"] = 35,
+        ["Defense"] = 30,
+        ["DamageReduction"] = 17.5
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0"
+}
+v2["Qin Shi Blindfold"] = v8
+local v9 = {
+    ["DisplayName"] = "Vampire Coat",
+    ["stats"] = {
+        ["Damage"] = 37.5,
+        ["Defense"] = 40,
+        ["DamageReduction"] = 15
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0"
+}
+v2["Alucard Coat"] = v9
+local v10 = {
+    ["DisplayName"] = "Dragon Slayer Sigil",
+    ["stats"] = {
+        ["Damage"] = 35,
+        ["Defense"] = 45,
+        ["DamageReduction"] = 15
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0"
+}
+v2["Ragna Sigil"] = v10
+local v11 = {
+    ["DisplayName"] = "Manipulator Haori",
+    ["stats"] = {
+        ["Damage"] = 40,
+        ["Defense"] = 50,
+        ["DamageReduction"] = 15
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0"
+}
+v2["Aizen Haori"] = v11
+local v12 = {
+    ["stats"] = {
+        ["Damage"] = 40,
+        ["Defense"] = 50,
+        ["DamageReduction"] = 15
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0"
+}
+v2["Hollow Mask"] = v12
+local v13 = {
+    ["stats"] = {
+        ["Damage"] = 20,
+        ["Defense"] = 30,
+        ["DamageReduction"] = 5
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0"
+}
+v2["Santa Hat"] = v13
+local v14 = {
+    ["stats"] = {
+        ["Damage"] = 45,
+        ["Defense"] = 60,
+        ["DamageReduction"] = 15
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0",
+    ["aura"] = "ShadowCloakAura"
+}
+v2["Shadow Cloak"] = v14
+local v15 = {
+    ["DisplayName"] = "Limitless Haori",
+    ["stats"] = {
+        ["Damage"] = 47.5,
+        ["Defense"] = 65,
+        ["DamageReduction"] = 15
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0"
+}
+v2["Gojo Haori"] = v15
+local v16 = {
+    ["stats"] = {
+        ["Damage"] = 50,
+        ["Defense"] = 70,
+        ["DamageReduction"] = 15
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0"
+}
+v2["Divine Wheel"] = v16
+local v17 = {
+    ["stats"] = {
+        ["Damage"] = 55,
+        ["Defense"] = 75,
+        ["DamageReduction"] = 15
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0",
+    ["aura"] = "SlimeWingsAura"
+}
+v2["Slime Wings"] = v17
+local v18 = {
+    ["stats"] = {
+        ["Damage"] = 50,
+        ["Defense"] = 65,
+        ["DamageReduction"] = 15
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0",
+    ["aura"] = "UltimateWingsAura"
+}
+v2["Ultimate Wings"] = v18
+local v19 = {
+    ["stats"] = {
+        ["Damage"] = 25,
+        ["Defense"] = 35,
+        ["DamageReduction"] = 10
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0"
+}
+v2["Valentine Heart"] = v19
+local v20 = {
+    ["stats"] = {
+        ["Damage"] = 42.5,
+        ["Defense"] = 55,
+        ["DamageReduction"] = 15
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0"
+}
+v2["Heart Wings"] = v20
+local v21 = {
+    ["DisplayName"] = "Golden Armor",
+    ["stats"] = {
+        ["Damage"] = 60,
+        ["Defense"] = 80,
+        ["DamageReduction"] = 15
+    },
+    ["attachTo"] = "Auto",
+    ["equipType"] = "FullBody",
+    ["image"] = "rbxassetid://0",
+    ["aura"] = "GilgameshArmorAura"
+}
+v2["Gilgamesh Armor"] = v21
+local v22 = {
+    ["DisplayName"] = "Demon King Outfit",
+    ["stats"] = {
+        ["Damage"] = 62.5,
+        ["Defense"] = 85,
+        ["DamageReduction"] = 10
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0",
+    ["equipType"] = "FullBody"
+}
+v2["Anos Outfit"] = v22
+local v23 = {
+    ["stats"] = {
+        ["Damage"] = 65,
+        ["Defense"] = 90,
+        ["DamageReduction"] = 15
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0",
+    ["equipType"] = "FullBody"
+}
+v2["Monarch Armor"] = v23
+local v24 = {
+    ["stats"] = {
+        ["Damage"] = 67.5,
+        ["Defense"] = 90,
+        ["DamageReduction"] = 15
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0",
+    ["equipType"] = "FullBody"
+}
+v2["Sun Armor"] = v24
+local v25 = {
+    ["stats"] = {
+        ["Damage"] = 72.5,
+        ["Defense"] = 100,
+        ["DamageReduction"] = 15
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0",
+    ["equipType"] = "FullBody"
+}
+v2["Manipulator Outfit"] = v25
+local v26 = {
+    ["stats"] = {
+        ["Damage"] = 70,
+        ["Defense"] = 95,
+        ["DamageReduction"] = 15
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0",
+    ["equipType"] = "FullBody"
+}
+v2["Maiden Outfit"] = v26
+local v27 = {
+    ["stats"] = {
+        ["Damage"] = 65,
+        ["Defense"] = 90,
+        ["DamageReduction"] = 15
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0",
+    ["equipType"] = "FullBody"
+}
+v2["Imperial Outfit"] = v27
+local v28 = {
+    ["stats"] = {
+        ["Damage"] = 75,
+        ["Defense"] = 100,
+        ["DamageReduction"] = 15
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0",
+    ["equipType"] = "FullBody"
+}
+v2["Alter Armor"] = v28
+local v29 = {
+    ["stats"] = {
+        ["Damage"] = 82.5,
+        ["Defense"] = 105,
+        ["DamageReduction"] = 15
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0",
+    ["equipType"] = "FullBody"
+}
+v2["Atomic Outfit"] = v29
+local v30 = {
+    ["stats"] = {
+        ["Damage"] = 77.5,
+        ["Defense"] = 102.5,
+        ["DamageReduction"] = 15
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0",
+    ["equipType"] = "FullBody"
+}
+v2["Warlord Armor"] = v30
+local v31 = {
+    ["stats"] = {
+        ["Damage"] = 80,
+        ["Defense"] = 105,
+        ["DamageReduction"] = 15
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0",
+    ["equipType"] = "FullBody"
+}
+v2["Abyssal Outfit"] = v31
+local v32 = {
+    ["stats"] = {
+        ["Damage"] = 85,
+        ["Defense"] = 107.5,
+        ["DamageReduction"] = 15
+    },
+    ["attachTo"] = "Auto",
+    ["image"] = "rbxassetid://0",
+    ["equipType"] = "FullBody"
+}
+v2["Moon Outfit"] = v32
+
+
+function Accessorys:GetBestDamageAccessory()
+    local player = game:GetService("Players").LocalPlayer
+    local storage = player.PlayerGui.InventoryPanelUI.MainFrame.Frame.Content.Holder.StorageHolder.Storage
+
+    local bestName = nil
+    local bestDamage = 0
+
+    for accName, data in pairs(v2) do
+        local searchKey = accName:gsub("%s+", "")
+        local searchDisplay = data.DisplayName and data.DisplayName:gsub("%s+", "")
+
+        for _, v in pairs(storage:GetChildren()) do
+            local invName = v.Name:gsub("%s+", "")
+
+            if invName:find(searchKey) or (searchDisplay and invName:find(searchDisplay)) then
+                if data.stats and data.stats.Damage and data.stats.Damage > bestDamage then
+                    bestDamage = data.stats.Damage
+                    bestName = accName
+                end
+            end
+        end
+    end
+
+    return bestName
+end
+
+return Accessorys
