d81d1cce2926b73afd1479ef3db06f8feb0bd4b4
Lmy77 <lmy77infinityx@gmail.com>
2026-05-04 19:55:08 -0300

    Update source.lua

--- games/Tower-of-Hell/source.lua
+++ games/Tower-of-Hell/source.lua
@@ -1,1 +1,3 @@
-game.Players.LocalPlayer:Kick("The itinerary is currently being revised; we appreciate your patience until everything is ready.")
+pcall(function()
+  game.Players.LocalPlayer:Kick("The script is currently being revised; we appreciate your patience until everything is ready.")
+end)
