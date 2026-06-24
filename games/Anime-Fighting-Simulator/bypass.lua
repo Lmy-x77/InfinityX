14cb68005313ac3b9a5c89a6c1166c80334edc13
Lmy77 <lmy77infinityx@gmail.com>
2026-06-24 17:54:18 -0300

    Update bypass.lua

--- games/Anime-Fighting-Simulator/bypass.lua
+++ games/Anime-Fighting-Simulator/bypass.lua
@@ -1,2 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Anime-Fighting-Simulator/modules/memory.lua"))();
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Anime-Fighting-Simulator/modules/safemode.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/library/Safe-Guard/script/NilInstances.lua"))();
