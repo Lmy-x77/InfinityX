c6cb37949cfb427d8855a461da1ba9a85ae8b3b5
Lmy77 <lmy77infinityx@gmail.com>
2026-07-14 11:20:35 -0300

    Update bypass.lua

--- games/Anime-Fighinting-Simulator/bypass.lua
+++ games/Anime-Fighinting-Simulator/bypass.lua
@@ -1,3 +1,35 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Anime-Fighting-Simulator/modules/memory.lua"))();
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Anime-Fighting-Simulator/modules/safemode.lua"))();
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/library/Safe-Guard/script/NilInstances.lua"))();
+safeloadstring = setmetatable({}, {
+  __call = function(self, url)
+  	assert(type(url) == "string", "Invalid URL")
+
+  	local ok, response = pcall(function()
+  	  return request({
+  	    Url = url,
+  	    Method = "GET"
+  	  })
+  	end)
+
+  	if not ok or not response or response.StatusCode ~= 200 then
+  	  return warn("Failed to fetch:", url)
+  	end
+
+  	local func, err = loadstring(response.Body)
+  	if not func then
+  	  return warn("Loadstring Error:", err)
+  	end
+
+  	setfenv(func, getgenv())
+
+  	local success, result = pcall(func)
+  	if not success then
+  	  warn("Runtime Error:", result)
+  	end
+
+    return result
+  end
+})
+
+
+safeloadstring("https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Anime-Fighting-Simulator/modules/memory.lua");
+safeloadstring("https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/scripts/games/Anime-Fighting-Simulator/modules/safemode.lua");
+safeloadstring("https://raw.githubusercontent.com/Lmy-x77/InfinityX/refs/heads/library/Safe-Guard/script/NilInstances.lua");
