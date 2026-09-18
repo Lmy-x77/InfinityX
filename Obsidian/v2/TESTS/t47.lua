9ba94fa6c4f0bc9d920be86c75f71f449122c0ab
Lmy77 <lmy77infinityx@gmail.com>
2026-09-18 19:53:28 -0300

    Update t47.lua

--- Obsidian/v2/TESTS/t47.lua
+++ Obsidian/v2/TESTS/t47.lua
@@ -10266,7 +10266,7 @@ do
             pcall(TeleportService.TeleportToPlaceInstance, TeleportService, game.PlaceId, game.JobId, LocalPlayer)
         end)
 
-        AddQuickAction(ActionsRow, "shuffle", "RJ smallest server", function()
+        AddQuickAction(ActionsRow, "shuffle", "RJ smallest", function()
             local PlaceId = game.PlaceId
             local JobId = game.JobId
 
